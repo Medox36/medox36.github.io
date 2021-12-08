c86ce7cde4d234ee7d46331cb5ee69364b026cd4
Medox36 <giuntinil@bzz.ch>
2021-12-08 20:51:07 +0100

    added scroll-progress-bar

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,9 @@
 window.onload = function () {
     mybutton = document.getElementById("to_top");
+    scrollbar = document.getElementById("progress_part");
 }
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction(); scrollBar()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -15,4 +16,11 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function scrollBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
 }
