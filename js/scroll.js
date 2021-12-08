bc72d84d08d48ecee9bca46101629d041563ec2a
Medox36 <giuntinil@bzz.ch>
2021-12-08 11:42:11 +0100

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -2,10 +2,6 @@ window.onload = function () {
     mybutton = document.getElementById("to_top");
 }
 
-
-
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -16,7 +12,6 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
