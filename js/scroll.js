a5724e0ac6a8302ff0d1902804f08648c93a265d
Medox36 <giuntinil@bzz.ch>
2021-12-09 09:25:36 +0100

    some changes :)

--- js/scroll.js
+++ js/scroll.js
@@ -13,7 +13,7 @@ function scrollFunction() {
     }
 }
 
-function topFunction() {
+function toTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
